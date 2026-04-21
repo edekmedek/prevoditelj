88cd97bb11912363b49326ee1e84d7cb68acdf91
Tvoje Ime <tvoj.email@primjer.com>
2026-04-21 12:05:01 +0200

    SW v7: brise stari cache pri aktivaciji

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
-﻿const CACHE = 'prevoditelj-v6';
+﻿const CACHE = 'prevodatelj-v7';
 
 self.addEventListener('install', e => {
     self.skipWaiting();
     e.waitUntil(
-        caches.open(CACHE).then(c => c.addAll(['./', './index.html', './style.css', './manifest.json']))
+        caches.open(CACHE).then(c => c.addAll(['./', './index.html', './style.css', './manifest.json', './avatar.jpg']))
     );
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(clients.claim());
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        ).then(() => clients.claim())
+    );
 });
 
 self.addEventListener('fetch', e => {
